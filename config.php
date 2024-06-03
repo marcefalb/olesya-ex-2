1693b0e96dc46db3679b2b715e9996e7275e2a3d
marcefalb <marcefalb@gmail.com>
2024-06-03 17:35:56 +0300

    fix 3

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-	define('db_host', 'localhost');
+	define('db_host', 'localhost:3306');
 	define('db_user', 'u2675069_default');
 	define('db_pass', '7pEQZQlzOv5a57zW');
 	define('db_name', 'u2675069_default');
